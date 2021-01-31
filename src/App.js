e121df1dbc3e111e75576828fcf3ccd57590413c
Salem_Eid <salem.eid@gmail.com>
2021-01-31 22:38:42 +0300

    added footer and testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import MyCalendar from './components/Calender';
+import StickyFooter from './components/Footer';
+
+import HeaderMUI from './components/HeaderMUI';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderMUI />
+
+      <MyCalendar />
+      <div>
+        <StickyFooter />
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,56 @@
+// import React, {Component} from "react";
+// import { FaAlignRight } from 'react-icons/fa';
+
+
+// // const Header = () => {
+
+//     // const [toggleState, setToggleState] = useState(false);
+
+//     // const Toggle = () => {
+//     //     setToggleState(prevState => !toggleState)
+//     // }
+
+//     // return (
+//     //     <>
+//     //     <div className="navBar">
+//     //         <button onClick={Toggle} >
+//     //             <FaAlignRight />
+//     //         </button>
+//     //         <ul className={!toggleState ? "nav-links show-nav" : "nav-links"}>
+//     //             <li href="#">Home</li>
+//     //             <li href="#">About Us</li>
+//     //             <li href="#">Contact</li>
+//     //         </ul>
+//     //     </div>
+//     //     </>
+//     // )
+// // }
+
+
+//     class Header extends Component {
+//         state = {
+//             toggle:false
+//         }
+//         Toggle = () => {
+//             this.setState({toggle:!this.state.toggle})
+//         }
+//         render() {
+//             return (
+//               <>
+//                   <div className="navBar">
+//                         <button onClick={this.Toggle}>
+//                             <FaAlignRight />
+//                         </button>
+//                         <ul className={this.state.toggle ? "nav-links show-nav" : "nav-links"}>
+//                             <li href="#">Home</li>
+//                             <li href="#">About us</li>
+//                             <li href="#">Contact</li>
+//                         </ul>
+//                   </div>
+//               </>
+//             );
+//         }
+//     }
+
+
+// export default Header;
